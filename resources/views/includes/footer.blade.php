b8ada855bf7d5d80d9c19c1fef1c5b6831bb9c37
ochi1000 <39088587+ochi1000@users.noreply.github.com>
2020-05-16 00:36:50 +0100

    social links

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,9 +10,9 @@
             <li><a href="mailto:support@psguy.com.ng">support@psguy.com.ng</a></li>
             <li><a href="">5 Okorie close New Haven, Enugu State</a></li>
             <li>
-                <a href="https://www.facebook.com/officialPSGUY/" style="margin-right: 0.5rem "><i style="color: #68a5fc; font-size:1.2rem" class="fab fa-twitter"></i></a>
+                <a href="https://twitter.com/officialPSGUY?s=08" style="margin-right: 0.5rem "><i style="color: #68a5fc; font-size:1.2rem" class="fab fa-twitter"></i></a>
                 <a href="https://instagram.com/official.psguy?igshid=1gqnzyavj027j" style="margin-right: 0.5rem "><i style="color: #f168fc; font-size:1.2rem" class="fab fa-instagram"></i></a>
-                <a href="https://twitter.com/officialPSGUY?s=08" style="margin-right: 0.5rem "><i style="color: #0064f4; font-size:1.2rem" class="fab fa-facebook"></i></a>
+                <a href="https://www.facebook.com/officialPSGUY/" style="margin-right: 0.5rem "><i style="color: #0064f4; font-size:1.2rem" class="fab fa-facebook"></i></a>
             </li>
         </div>
         <div class="col-lg-6">
